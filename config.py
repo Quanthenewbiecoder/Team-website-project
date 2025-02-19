05c0f73f3515989f9550d2f289f995ce807db240
Quanthenewbiecoder <122609638+Quanthenewbiecoder@users.noreply.github.com>
2025-02-19 15:59:03 +0000

    Change to sqlite to use for developing

--- config.py
+++ config.py
@@ -1,13 +1,9 @@
 import os
-from urllib.parse import quote_plus
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
     
-    # MySQL database credentials
-    user = os.environ.get('DB_USER', 'Team')
-    password = os.environ.get('DB_PASSWORD', 'Database8080')
-    host = os.environ.get('DB_HOST', 'localhost')
-    db_name = os.environ.get('DB_NAME', 'team')
-    SQLALCHEMY_DATABASE_URI = f'mysql://{user}:{quote_plus(password)}@{host}/{db_name}'
+    # SQLite database path
+    db_path = os.environ.get('DB_PATH', os.path.join(os.getcwd(), 'divine.db'))
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{db_path}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
