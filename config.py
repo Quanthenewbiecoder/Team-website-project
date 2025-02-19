da6d00211f4887352d40bb2029f7b1435aaf3fff
Quanthenewbiecoder <122609638+Quanthenewbiecoder@users.noreply.github.com>
2025-02-19 15:55:27 +0000

    minor update

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
     
     # MySQL database credentials
-    user = os.environ.get('DB_USER', 'Nas')
+    user = os.environ.get('DB_USER', 'Team')
     password = os.environ.get('DB_PASSWORD', 'Database8080')
-    host = os.environ.get('DB_HOST', '10.76.4.182')
+    host = os.environ.get('DB_HOST', 'localhost')
     db_name = os.environ.get('DB_NAME', 'team')
     SQLALCHEMY_DATABASE_URI = f'mysql://{user}:{quote_plus(password)}@{host}/{db_name}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
