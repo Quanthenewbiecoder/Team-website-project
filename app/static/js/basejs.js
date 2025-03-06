30b680dfdebd7bd233a84e8f5af72a3761aeb2f9
Nas56 <abdinasirsharif98@gmail.com>
2025-03-06 15:22:37 +0000

    bug fixes

--- app/static/js/basejs.js
+++ app/static/js/basejs.js
@@ -1,8 +1,16 @@
 window.addEventListener('scroll', () => {
   const navbar = document.querySelector('.navbar');
-  if (window.scrollY > 500) {
+  if (window.scrollY > 80) {
     navbar.classList.add('sticky');
   } else {
     navbar.classList.remove('sticky');
+  }
+});
+
+window.addEventListener('resize', () => {
+  const menu = document.querySelector('.menu');
+  if (window.innerWidth > 900 && menu.classList.contains('show')) {
+    menu.classList.remove('show');
+    document.removeEventListener('click', closeMenuOutside);
   }
 });
