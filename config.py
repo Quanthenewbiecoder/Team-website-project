8c3c35c172c4c077544de3b4fe2dbabfaa3385e5
Quan Duong <122609638+Quanthenewbiecoder@users.noreply.github.com>
2025-03-16 21:49:48 +0000

    the admin add products works

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
     # Dynamically set the upload folder path
-    UPLOAD_FOLDER = os.path.join(BASE_DIR, "static", "images")
+    UPLOAD_FOLDER = os.path.join(BASE_DIR, "app", "static", "images")
 
     # Ensure the upload folder exists
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
